54151579006bfc472103f3aead2ae590a9df0de2
Sercan Çintosun <sercancintosun000@gmail.com>
2026-09-23 19:00:35 +0300

    Onion arc altyapısı oluşturuldu.

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ETicaret.API.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProduct();
+    }
+}
